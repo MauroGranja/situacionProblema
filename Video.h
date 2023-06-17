9db39c6d5ef4fc7dedd5509913acb1156b63215f
MauroGranja <135269951+MauroGranja@users.noreply.github.com>
2023-06-17 15:08:30 -0600

    Update Video.h

--- Video.h
+++ Video.h
@@ -1,1 +1,32 @@
+#ifndef VIDEO_H
+#define VIDEO_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Video
+{
+  string id;
+  string nombre;
+  float duracion;
+  vector<string> generos;
+  float calificacion;
+  string fechaEstreno;
+public:
+  Video (string id, string nombre, float duracion, vector<string> generos, float calificacion, string fechaEstreno);
+  void setCalificacion(float calificacion);
+  string obtenerId();
+  string obtenerNombre();
+  float obtenerDuracion();
+  vector<string> obtenerGeneros();
+  float obtenerCalificacion();
+  string obtenerFechaEstreno();
+  bool tieneGenero(string genero);
+  virtual void imprimirInformacion();
+  void imprimirGeneros();
+};
+
+#endif // !VIDEO_H
